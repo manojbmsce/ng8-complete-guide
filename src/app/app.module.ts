3a7491a2cf37c6864fb385840072033b78325763
Manoj Manoj <manoj@hexad.de>
2019-08-13 15:45:40 +0200

    Shopping-recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping/shopping-list/shopping-edit/shopping-edit.component';
+import { RecipesComponent} from './recipes/recipes.component';
+import { RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
